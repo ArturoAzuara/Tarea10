3d5386a957026c720dbac8481d754761ce98c21a
ArturoAzuara <arturoazuara92@aragon.unam.mx>
2023-05-06 18:50:24 -0600

    Tarea 10

---
+++ Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        //Ventana ventana = new Ventana("Layouts");
+        //VentanaGrid ventanagrid = new VentanaGrid("Ventana Layout Cuadricula");
+        VentanaSwing ventanaSwing = new VentanaSwing("Centigrados a Farenheid");
+    }
+}
